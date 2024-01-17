c83bd58711e58267ecf1e436c4ddc045365086e1
Touka Raafat <107259388+Toukaraafat@users.noreply.github.com>
2024-01-17 12:06:37 +0200

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieServiceService } from '../services/movie-service.service';
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ SearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
