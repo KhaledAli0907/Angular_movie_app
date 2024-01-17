9a7ea961b7959fa9d08611c1c22812dc908592e6
Touka Raafat <107259388+Toukaraafat@users.noreply.github.com>
2024-01-17 21:23:31 +0200

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieServiceService } from '../services/movie-service.service';
 import { SearchComponent } from '../search/search.component';
+import { MovieCardComponent } from '../movie-card/movie-card.component';
+import { Router ,RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ SearchComponent],
+  imports: [ SearchComponent, MovieCardComponent,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
   constructor(private moviesService: MovieServiceService) {}
 
-  movies: any;
-
+  movies:any;
+  movie: any;
   ngOnInit(): void {
-    this.moviesService.getMoviePage('2').subscribe((movies) => {
+    this.moviesService.getMoviePage("1").subscribe((movies: any) => {
       this.movies = movies;
-      console.log(movies);
+      console.log(movies.results);
     });
   }
+
 }
