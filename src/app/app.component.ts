fa1c83a897dffae84b8614a6e66fb91d317645f4
Touka Raafat <107259388+Toukaraafat@users.noreply.github.com>
2024-01-17 12:03:54 +0200

    search component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent, HeaderComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
