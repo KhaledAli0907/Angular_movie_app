e7a932cf1c26f4b4f43432b3680ed6d7c84cf803
Touka Raafat <107259388+Toukaraafat@users.noreply.github.com>
2024-01-17 12:16:58 +0200

    components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component: HomeComponent,
+    },
+    {
+        path: 'movie-details/:id',
+        component: MovieDetailsComponent
+    },
+    {
+        path: "**",
+        component: NotFoundComponent
+    }
+];
