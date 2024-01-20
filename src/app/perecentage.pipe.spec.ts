c89fa09434c1ba5993e0668f906de6742bd01216
Touka Raafat <107259388+Toukaraafat@users.noreply.github.com>
2024-01-20 19:11:03 +0200

    progress design

---
+++ src/app/perecentage.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PerecentagePipe } from './perecentage.pipe';
+
+describe('PerecentagePipe', () => {
+  it('create an instance', () => {
+    const pipe = new PerecentagePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
